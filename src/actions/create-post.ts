123b4d518ae9041a9212b6c15dcb8284de09312c
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-22 21:19:30 -0800

    create post form validation

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,79 @@
 'use server'
 
-export async function createPost() {
+import { auth } from '@/auth'
+import { z } from 'zod'
+import type { Post } from '@prisma/client'
+import paths from '@/paths'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+})
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[]
+    content?: string[]
+    _form?: string[]
+  }
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  })
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
+  }
+
+  const session = await auth()
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this'],
+      },
+    }
+  }
+
+  // let post: Post
+  // try {
+  //   post = await db.post.create({
+  //     data: {
+  //       title: result.data.title,
+  //       content: result.data.content,
+  //     },
+  //   })
+  // } catch (err: unknown) {
+  //   if (err instanceof Error) {
+  //     return {
+  //       errors: {
+  //         _form: [err.message],
+  //       },
+  //     }
+  //   } else {
+  //     return {
+  //       errors: {
+  //         _form: ['Something went wrong'],
+  //       },
+  //     }
+  //   }
+  // }
+
+  // revalidatePath('/')
+  // redirect(paths.topicShow(topic.slug))
+
+  return {
+    errors: {},
+  }
   // TODO: revalidate topic show page
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -6,17 +6,24 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Textarea,
 } from '@nextui-org/react'
 import FormButton from '../common/form-button'
+import { useFormState } from 'react-dom'
+import * as actions from '@/actions'
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  })
+
   return (
     <Popover placement='left'>
       <PopoverTrigger>
         <Button color='primary'>Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action=''>
+        <form action={action}>
           <div className='flex flex-col gap-4 p-4 w-80'>
             <h3 className='text-lg'>Create a Post</h3>
             <Input
@@ -24,12 +31,16 @@ export default function PostCreateForm() {
               label='Title'
               labelPlacement='outside'
               placeholder='Title'
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
             />
-            <Input
+            <Textarea
               name='content'
               label='Content'
               labelPlacement='outside'
               placeholder='Content'
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
             />
             <FormButton>Create Post</FormButton>
           </div>
