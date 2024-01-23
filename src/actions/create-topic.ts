f467e561a2dfdc1b9ac4495ec10e9e7cdb2edcfb
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-22 20:07:59 -0800

    set up zod validation on form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,26 @@
 'use server'
 
+import { z } from 'zod'
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowecase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+})
+
 export async function createTopic(formData: FormData) {
-  const name = formData.get('name')
-  const description = formData.get('description')
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  })
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors)
+  }
 
   // TODO: revalidate home page
 }
