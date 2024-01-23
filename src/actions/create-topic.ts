54139ff43b95a7d73db6ab297d8ec35c1e127ce9
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-22 20:39:31 -0800

    add generic error and connection to db to create topic

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,12 @@
 'use server'
 
+import { auth } from '@/auth'
 import { z } from 'zod'
+import type { Topic } from '@prisma/client'
+import paths from '@/paths'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
 const createTopicSchema = z.object({
   name: z
@@ -16,6 +22,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[]
     description?: string[]
+    _form?: string[]
   }
 }
 
@@ -34,8 +41,39 @@ export async function createTopic(
     }
   }
 
-  return {
-    errors: {},
+  const session = await auth()
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this'],
+      },
+    }
   }
-  // TODO: revalidate home page
+
+  let topic: Topic
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    })
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      }
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      }
+    }
+  }
+
+  revalidatePath('/')
+  redirect(paths.topicShow(topic.slug))
 }
